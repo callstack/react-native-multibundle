3caebae674ef41716914634a415f4603db3afb51
Paweł Trysła <zamotany.oss@gmail.com>
2020-02-14 19:35:58 +0100

    move code from registerBlankComponent.js to dll.js

--- Example/src/dll.js
+++ Example/src/dll.js
@@ -1,1 +1,21 @@
-import "./registerBlankComponent";
+import React, { useEffect, useState } from "react";
+import { AppRegistry, View, Text } from "react-native";
+import Multibundle from "react-native-multibundle";
+
+AppRegistry.registerComponent("Example", () => () => {
+  const [RootComponent, setRootComponent] = useState(() => () => (
+    <View>
+      <Text>initial</Text>
+    </View>
+  ));
+
+  useEffect(() => {
+    (async () => {
+      await Multibundle.loadBundle("host");
+
+      setRootComponent(() => Multibundle.getBundleExport("host"));
+    })();
+  }, []);
+
+  return <RootComponent />;
+});
