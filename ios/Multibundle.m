c25aa2a230ba5fc2d6cca455d7fbad809685aeea
maciej simka <mcj.simka@gmail.com>
2020-02-14 13:37:25 +0100

    feat: add initial iOS

--- ios/Multibundle.m
+++ ios/Multibundle.m
@@ -1,14 +1,30 @@
 #import "Multibundle.h"
-
+#import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
 
 @implementation Multibundle
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(loadBundle:(NSString *)bundleName
+    bundleId:(nonnull NSNumber *)bundleId
+    sync:(BOOL)sync
+    resolver:(RCTPromiseResolveBlock)resolve
+    rejecter:(RCTPromiseRejectBlock)reject)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    RCTBridge *bridge = [RCTBridge alloc];
+
+    @try
+    {
+        NSURL *u = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+        [bridge registerSegmentWithId:bundleId path:u.absoluteString];
+
+        resolve(nil);
+        
+    } @catch (NSException * exception)
+    {
+        reject(@"error", @"error", nil);
+    }
 }
 
 @end
